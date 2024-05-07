52b12a37df3d2d9c0b55533d4b1e33632dd822db
Julia March <101819212+juliamrch@users.noreply.github.com>
2024-05-08 00:00:25 +0200

    chore/ui: create Greeting island with client directive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-demo.cleverapps.io/"
+  site: "https://astro-demo.cleverapps.io/",
+  integrations: [
+    preact({})]
 });
-
