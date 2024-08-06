7baadcc8fdf69fe27dc421b72430282dbb4151e3
Julia March <101819212+juliamrch@users.noreply.github.com>
2024-08-06 11:48:08 +0200

    chore: config server options

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-demo.cleverapps.io/",
+  server: {
+    port: 8080,
+    host: true
+  },
   integrations: [
     preact({})]
 });
